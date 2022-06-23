ab6cb06b47cca9878237956d2f5fe2a9e3e82095
Oskar Paulsson <oskar@paulsson.dev>
2022-06-23 23:52:44 +0200

    Start of something wonderful...

--- src/server.py
+++ src/server.py
@@ -1,2 +1,14 @@
-# Comment
-# Test
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+def run()
+    """Entrypoint for python server"""
+    server_address = ("0.0.0.0", 8000)
+    httpd = server_class(server_address, handler_class)
+    print("launching server...")
+    httpd.serve_forever()
+
+def main():
+    run()
+
+if __name__=="__main__":
+    main()
